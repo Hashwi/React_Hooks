9e1b1f01092752f306541fcc164717cdc261082a
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-07-23 15:06:02 +0200

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
+import React, { useEffect, useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+ const [user, setUser]  = useState('Hash')
+ 
+//  useEffect(()=> {
+
+//  }, [])
+
+ const clickHandler = () => {
+  setUser(user)
+ }
+
+ 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello</h1>
+        {user}
+        <button onClick={clickHandler}>Click</button>
       </header>
     </div>
   );
